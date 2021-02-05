d6601168192e36b1969a515acf87ebdff7261a3d
derek <derekle.creative@gmail.com>
2021-02-05 13:38:38 -0600

    commit tree 0917b78a659a3ec8599060d2b1d8025ec509a507

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,33 @@
-# Add your code here
+class Dog
+    @@all = Array.new
+
+    def initialize(name)
+        p "[initialize]"
+        @name = name
+        save
+    end
+    attr_accessor :name
+    def self.all
+        p "[all]"
+        @@all
+    end
+
+    def self.clear_all
+        p "[clear_all]"
+        until @@all.length == 0
+            @@all.pop
+        end
+    end
+
+    def self.print_all
+        #p "[print_all]"
+        @@all.each do |x|
+            puts x.name
+        end
+    end
+
+    def save
+        p "[save]"
+        @@all << self
+    end
+end
